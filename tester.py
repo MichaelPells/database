e33ac9b5ce80eb1e965fe29d48ae6d0fbc6eeb8e
MichaelPells <themichaelpells@gmail.com>
2025-10-23 06:21:55 +0100

    Implementing and testing conditional typing

--- tester.py
+++ tester.py
@@ -179,10 +179,10 @@ class String1(String):
     
 class String2(String):
     def check(data):
-        return data.endswith("lo")
+        return data.startswith("hel")
 
 db =  Database()
-columns = [("id", Number), ("email", AND(String1, String2)), "firstname", "middlename", "surname", "gender", "country", "phone", "isstudent", "school", "referrer"]
+columns = [("id", Number), ("email", OR(String1, String2)), "firstname", "middlename", "surname", "gender", "country", "phone", "isstudent", "school", "referrer"]
 entries = [[int(y.strip()) if y.isdigit() else (y.strip() if y.strip() else Null())
             for y in x.split(",")]
             for x in open("test_data_1.csv").read().splitlines()]
